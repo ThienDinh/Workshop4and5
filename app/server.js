845f191386a36597dec8ad0c0496ecfbcb88f924
Thien Dinh <tdinhcs@gmail.com>
2016-10-23 19:17:47 -0400

    fb4

--- app/server.js
+++ app/server.js
@@ -9,3 +9,20 @@ function emulateServerReturn(data, cb) {
     cb(data);
   }, 4);
 }
+
+function getFeedItemSync(feedItemId) {
+	var feedItem = readDocument('feedItems', feedItemId);
+	feedItem.likeCounter = feedItem.likeCounter.map((id)=> readDocument('users', id));
+	feedItem.contents.author = readDocument('users', feedItem.contents.author);
+	feedItem.comments.forEach((comment) => {
+		comment.author = readDocument('user', comment.author);
+	});
+	return feedItem;
+}
+
+export function getFeedData(user, cb) {
+	var userData = readDocument('users', user);
+	var feedData = readDocument('feeds', userData.feed);
+	feedData.contents = feedData.contents.map(getFeedItemSync);
+	emulateServerReturn(feedData, cb);
+}
